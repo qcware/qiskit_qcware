114b1241c2a94360d68391bbd966b931a3e14363
Vic Putz <vbputz@gmail.com>
2020-10-22 16:12:13 -0500

    Changes working toward running a dummy circuit
    Mostly concerning population of results and making notes
    pointing toward appropriate qiskit classes.

---
+++ qiskit_qcware/qcware_backend.py
@@ -0,0 +1,106 @@
+import qcware
+from qiskit.providers import BackendV1, Provider
+from qiskit.providers.models import BackendConfiguration
+from qiskit.circuit import QuantumCircuit
+from quasar import Circuit as QuasarCircuit
+from quasar import ProbabilityHistogram as QuasarProbabilityHistogram
+from quasar import CountHistogram as QuasarCountHistogram
+from quasar import QuasarSimulatorBackend
+from typing import Union, List
+from uuid import uuid4
+from qcware_job import QcwareJob
+
+# based in part on the documentation located at
+# https://github.com/Qiskit/qiskit-tutorials/blob/master/legacy_tutorials/terra/6_creating_a_provider.ipynb
+# and from the AQT backend, https://github.com/qiskit-community/qiskit-aqt-provider
+# also look at https://github.com/Qiskit/qiskit-terra/blob/master/qiskit/providers/basicaer/qasm_simulator.py
+# for example of local simulator backend
+
+EVERY_JOB_ID = 42
+
+
+class LocalQuasarBackend(BackendV1):
+    """
+    A qiskit wrapper backend for the qcware-quasar QuasarSimulator
+    backend, providing a local simulator for testing and validation
+    of circuit translation, etc.
+    """
+    def __init__(self, provider: Provider = None):
+        configuration = {
+            'backend_name': 'local_quasar_simulator',
+            'backend_version': '0.0.0',
+            'url': 'http://www.qcware.com',
+            'simulator': True,
+            'local': True,
+            'coupling_map': None,
+            'description':
+            "Wrapper for the QuasarSimulator classical testing simulator",
+            'basis_gates': ['h', 'x'],
+            'memory': True,
+            'n_qubits': 30,
+            'conditional': False,
+            'max_shots': 100000,
+            'open_pulse': False,
+            'gates': [{
+                'name': 'TODO',
+                'parameters': [],
+                'qasm_def': 'TODO'
+            }],
+        }
+
+        super().__init__(
+            configuration=BackendConfiguration.from_dict(configuration),
+            provider=provider)
+
+    def _qiskit_circuit_to_quasar_circuit(self, c: QuantumCircuit)->QuasarCircuit:
+        result = Circuit()
+        result.H(0).CX(0,1)
+        return result
+
+    def _execute_quasar_circuit_measurement(self, c: QuasarCircuit)->QuasarHistogram:
+        backend = QuasarSimulatorBackend()
+        return backend.run_measurement(c)
+
+    def _quasar_histogram_to_qiskit(self, h: QuasarProbabilityHistogram)->ExperimentResult:
+        count_histogram = h.to_count_histogram()
+        # count histogram is a dict of integer representations of state to counts
+        qiskit_counts = { hex(k): v for k,v in count_histogram.items() }
+        experiment_data = ExperimentResultData(counts=qiskit_counts)
+        experiment_result = ExperimentResult(shots=count_histogram.nmeasurement,
+                                             success=True,
+                                             data=experiment_data)
+        # meas_level = MeasLevel.CLASSIFIED, meas_return = MeasLevel.AVERAGE ?
+        # TODO must set jobj_id and job_id
+
+    def run(self, run_input: Union[QuantumCircuit,
+                                   List[QuantumCircuit]]) -> QcwareJob:
+        """
+        Run a circuit or list of circuits
+
+        Returns: QcwareJob
+        """
+        # the AQT backend submits the call here to get an ID.  This is also
+        # duplicated in the AqtJob.submit
+        # function
+        # in qiskit.execute, you see blocks like
+        #    start_time = time()
+        #    job = backend.run(...)
+        #    end_time = time()
+        #
+        # implying that run() should handle everything, so the use of
+        # submit/cancel/status in job is mildly confusing.  They log
+        # "submission time" so perhaps only submission is counted and
+        # the job is used to look up the result in qasm_simulator,
+        # this creates the job, assigns a uuid for the job id,
+        # provides a call back function to actually run the job
+        # (backend._run_job) and then "submits" it; the job class has
+        # a process pool executor and it's the job that actually
+        # executes the result.
+        job = QcwareJob(self, uuid4(), qobj=qobj)
+        # actually fill in the result here
+        # currently only handling one circuit
+        job._result = Result(backend_name=self._configuration.backend_name,
+                             backend_version=self._configuration.backend_version,
+                             qobj_id=None,
+                             self.job_id = job.job_id())
+        return job
---
+++ qiskit_qcware/qcware_job.py
@@ -0,0 +1,53 @@
+from qiskit.providers import (JobV1, JobError, JobTimeoutError, Backend,
+                              JobStatus)
+from qiskit.result import Result
+from qiskit.qobj import Qobj
+from dpcontracts import require
+
+# using base from https://github.com/Qiskit/qiskit-terra/blob/master/qiskit/providers/job.py
+
+
+class QcwareJob(JobV1):
+
+    # The documentation says the job "allows to synchronize different executions of the simulator",
+    # but this is not completely clear.
+    # https://qiskit.org/documentation/stubs/qiskit.providers.BaseJob.html?highlight=basejob#qiskit.providers.BaseJob
+    # job_id(str) is "a unique id in the context of the backend used to run the job
+    def __init__(self,
+                 backend: Backend,
+                 job_id: str,
+                 access_token=None,
+                 qobj: Qobj = None):
+        super().__init__(backend, job_id)
+        self._result = None
+
+    @require("Job must be in a final state", lambda args: args.self.in_final_state())
+    def result(self)->Result:
+        """
+        Return the results of the job.
+        """
+        # can wait for the result here (AQT does this)
+        # but the intent is to be asynchronous
+        return self._result
+
+    def cancel(self):
+        """
+        Attempt to cancel the job (NOP for local simulator,
+        raises NotImplementedError)
+        """
+        super().cancel()
+
+    def status(self) -> JobStatus:
+        """
+        Return the status of the job, among the values of JobStatus
+        """
+        # see qiskit.providers.jobstatus
+        # https://github.com/Qiskit/qiskit-terra/blob/900c048df8d763ec1b43cb0d32afb3e6ef5c48ae/qiskit/providers/jobstatus.py
+        pass
+
+    def submit(self):
+        """
+        Submit the job to the backend for execution.  Deprecated; the backend should
+        be handling the submission
+        """
+        raise NotImplementedError
